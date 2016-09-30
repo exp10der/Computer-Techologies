710e3a144debce0b3d62a7ed349a330b9cceba36
Konstantin <konstantin.writing.code@gmail.com>
2016-09-30 14:41:26 -0700

    Added queue interface

---
+++ GZipTest/Queue/BlockingQueue.cs
@@ -0,0 +1,48 @@
+﻿namespace GZipTest.Queue
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class BlockingQueue<T> : IDisposable
+    {
+        private readonly Queue<T> _queue;
+        private readonly object _queueLockObject = new object();
+
+        public BlockingQueue(int size)
+        {
+            Initialize();
+        }
+
+        void IDisposable.Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetConsumingEnumerable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object SyncRoot() => _queueLockObject;
+
+        public void CompleteAdding()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
