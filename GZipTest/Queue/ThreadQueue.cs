861129e88e06b519004e91c56f9bbca5131a1482
Konstantin <konstantin.writing.code@gmail.com>
2016-09-30 15:04:27 -0700

    Add ThreadQueue

---
+++ GZipTest/Queue/ThreadQueue.cs
@@ -0,0 +1,39 @@
+﻿namespace GZipTest.Queue
+{
+    using System;
+    using System.Threading;
+
+    internal class ThreadQueue
+    {
+        private const int _boundedCapacity = 8;
+        private readonly BlockingQueue<Action> _task;
+
+        public ThreadQueue(int workerCount)
+        {
+            _task = new BlockingQueue<Action>(_boundedCapacity);
+
+            for (var i = 0; i < workerCount; i++)
+            {
+                new Thread(Consume).Start();
+            }
+        }
+
+        public void Dispose()
+        {
+            _task.CompleteAdding();
+        }
+
+        public void Enqueue(Action action)
+        {
+            _task.Add(action);
+        }
+
+        private void Consume()
+        {
+            foreach (var task in _task.GetConsumingEnumerable())
+            {
+                task();
+            }
+        }
+    }
+}
