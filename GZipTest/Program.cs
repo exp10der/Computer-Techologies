0ea144517db2d26d99ed2fc2a9d4353a2b7eb7da
Konstantin <konstantin.writing.code@gmail.com>
2016-09-30 15:29:06 -0700

    ThreadQueue simple use case

--- GZipTest/Program.cs
+++ GZipTest/Program.cs
@@ -1,9 +1,32 @@
 ﻿namespace GZipTest
 {
+    using System;
+    using System.Threading;
+    using Queue;
+
     internal class Program
     {
         private static void Main(string[] args)
         {
+            var countIter = 30;
+            var remainingWorkThread = countIter;
+            var context = new ThreadQueue(8);
+
+            using (var mre = new ManualResetEvent(false))
+            {
+                for (var i = 0; i < countIter; i++)
+                {
+                    var index = i;
+
+                    context.Enqueue(() => { Console.WriteLine(index); });
+
+                    if (Interlocked.Decrement(ref remainingWorkThread) == 0)
+                        mre.Set();
+                }
+
+                // Wait for all threads to complete.
+                mre.WaitOne();
+            }
         }
     }
 }
