ae4700e51cdcd8fbb6a425917e742500bfb23000
Konstantin <konstantin.writing.code@gmail.com>
2016-10-02 14:06:32 -0700

    CreateFileHandle with buffer/sequential_scan

---
+++ GZipTest/Files/File.cs
@@ -0,0 +1,41 @@
+﻿namespace GZipTest.Files
+{
+    using System;
+    using System.ComponentModel;
+    using System.IO;
+    using System.Runtime.InteropServices;
+    using Microsoft.Win32.SafeHandles;
+    using Win32;
+
+    internal class File
+    {
+        public static FileStream OpenRead(string path, int bufferSize, bool sequential)
+        {
+            return OpenRead(path, FileMode.Open, FileAccess.Read, FileShare.None, bufferSize, sequential);
+        }
+
+        private static FileStream OpenRead(string path, FileMode mode, FileAccess access, FileShare share,
+            int bufferSize, bool sequential)
+        {
+            return new FileStream(CreateFileHandle(path, mode, access, share, sequential), access, bufferSize, false);
+        }
+
+        private static SafeFileHandle CreateFileHandle(string path, FileMode mode, FileAccess access, FileShare share,
+            bool sequential)
+        {
+            var flags = sequential ? WinNT.FILE_FLAG_SEQUENTIAL_SCAN : 0;
+
+            var fileHandle = WinNT.CreateFile(path, (uint) access, (uint) share, IntPtr.Zero, (uint) mode, (uint) flags,
+                IntPtr.Zero);
+
+
+            if (!fileHandle.IsInvalid) return fileHandle;
+
+            // Check for errors.
+            var lastWin32Error = Marshal.GetLastWin32Error();
+            throw new Win32Exception(
+                lastWin32Error,
+                $"Error {lastWin32Error} creating file handle for file path '{path}': {new Win32Exception(lastWin32Error).Message}");
+        }
+    }
+}
--- GZipTest/Program.cs
+++ GZipTest/Program.cs
@@ -2,8 +2,18 @@
 {
     internal class Program
     {
+        //  C:\Work\Node.js Design Patterns - Second Edition.pdf
+        //  \\MachineName\Work\Node.js Design Patterns - Second Edition.pdf
+        //  \\?\C:\Work\Node.js Design Patterns - Second Edition.pdf
+        //  \\?\UNC\MachineName\Work\Node.js Design Patterns - Second Edition.pdf
+        //  \\127.0.0.1\Work\Node.js Design Patterns - Second Edition.pdf
+        //  \\?\UNC\127.0.0.1\Work\Node.js Design Patterns - Second Edition.pdf
+
+
         private static void Main(string[] args)
         {
+            var test = Files.File.OpenRead(@"\\?\C:\Work\Node.js Design Patterns - Second Edition.pdf", 4096, false);
+
         }
     }
 }
