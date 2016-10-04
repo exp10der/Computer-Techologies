b986b4644f3f9d780b4eeb570809e79f99a96b67
Konstantin <konstantin.writing.code@gmail.com>
2016-10-04 13:06:09 -0700

    add abstraction StreamChunk

---
+++ GZipTest/Files/StreamChunk.cs
@@ -0,0 +1,14 @@
+﻿namespace GZipTest.Files
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    internal class StreamChunk<T>
+    {
+        public static IEnumerable<T> SplitStream(Stream stream, int chunkLength)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
