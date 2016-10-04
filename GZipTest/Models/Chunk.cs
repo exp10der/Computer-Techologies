6c076f46061c0721406fcdbe6db02e67aa85085f
Konstantin <konstantin.writing.code@gmail.com>
2016-10-04 13:09:40 -0700

    Add Model Chunk

---
+++ GZipTest/Models/Chunk.cs
@@ -0,0 +1,14 @@
+﻿namespace GZipTest.Models
+{
+    internal class Chunk
+    {
+        public readonly byte[] Data;
+        public readonly long Offset;
+
+        public Chunk(byte[] data, long offset)
+        {
+            Data = data;
+            Offset = offset;
+        }
+    }
+}
