ba51a04b8bfdac5d5b91394abf06434cea9208cd
Konstantin <konstantin.writing.code@gmail.com>
2016-10-09 12:41:22 -0700

    added ActionBlock

---
+++ GZipTest/Dataflow/ActionBlock.cs
@@ -0,0 +1,55 @@
+﻿namespace GZipTest.Dataflow
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using Queue;
+
+    internal class ActionBlock<TSource>
+    {
+        private readonly BlockingQueue<TSource> _inputQueue;
+        private readonly Action<Exception> _onError;
+        private readonly IEnumerable<TSource> _source;
+        private Exception _error;
+
+        public ActionBlock(IEnumerable<TSource> source, int capacity, Action<Exception> onError)
+        {
+            _source = source;
+            _onError = onError;
+            _inputQueue = new BlockingQueue<TSource>(capacity);
+        }
+
+        public IEnumerable<TSource> LinkTo()
+        {
+            var task = new Thread(() =>
+            {
+                try
+                {
+                    foreach (var element in _source)
+                    {
+                        //http://www.albahari.com/threading/part4.aspx
+                        Thread.MemoryBarrier();
+                        if (_error != null) break;
+
+                        _inputQueue.Add(element);
+                    }
+                }
+                catch (Exception e)
+                {
+                    if (Interlocked.CompareExchange(ref _error, e, null) == null)
+                        _onError(e);
+                }
+                finally
+                {
+                    _inputQueue.CompleteAdding();
+                }
+            });
+
+            task.IsBackground = true;
+
+            task.Start();
+
+            return _inputQueue.GetConsumingEnumerable();
+        }
+    }
+}
--- GZipTest/Dataflow/CompressorFlow.cs
+++ GZipTest/Dataflow/CompressorFlow.cs
@@ -2,12 +2,20 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Threading;
     using Models;
 
     internal static class CompressorFlow
     {
-        public static IEnumerable<Chunk> Transform(this IEnumerable<Chunk> source, Func<Chunk,Chunk> compressSelector)
+        public static IEnumerable<Chunk> Transform(this IEnumerable<Chunk> source, Func<Chunk, Chunk> compressSelector)
         {
+            Exception error = null;
+
+            Action<Exception> onError = e => Interlocked.CompareExchange(ref error, e, null);
+
+            // https://msdn.microsoft.com/en-us/library/ff963548.aspx
+            var actionBlock = new ActionBlock<Chunk>(source, 8, onError);
+            
             throw new NotImplementedException();
         }
     }
